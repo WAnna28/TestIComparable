f981deb4971bb287f76468e6cb73d47b76b5609f
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-09-23 03:14:16 +0400

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+
+namespace TestIComparable.Models
+{
+    public class Car : IComparable
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int CarID { get; set; }
+
+        public Car(string name, int currSp, int id)
+        {
+            CurrentSpeed = currSp;
+            PetName = name;
+            CarID = id;
+        }
+
+        // Version 1
+        // IComparable implementation.
+        //int IComparable.CompareTo(object obj)
+        //{
+        //    if (obj is Car temp)
+        //    {
+        //        if (this.CarID > temp.CarID)
+        //        {
+        //            return 1;
+        //        }
+        //        if (this.CarID < temp.CarID)
+        //        {
+        //            return -1;
+        //        }
+        //        return 0;
+        //    }
+        //    throw new ArgumentException("Parameter is not a Car!");
+        //}
+
+        // Version 2
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj is Car temp)
+            {
+                return this.CarID.CompareTo(temp.CarID);
+            }
+            throw new ArgumentException("Parameter is not a Car!");
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // A general way to compare two objects.
+    ///////////////////////////////////////////////////////////////////////////////
+    //interface IComparer
+    //{
+    //    int Compare(object o1, object o2);
+    //}
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    public class PetNameComparer : IComparer
+    {
+        // Test the pet name of each object.
+        int IComparer.Compare(object o1, object o2)
+        {
+            if (o1 is Car t1 && o2 is Car t2)
+            {
+                return string.Compare(t1.PetName, t2.PetName, StringComparison.OrdinalIgnoreCase);
+            }
+            else
+            {
+                throw new ArgumentException("Parameter is not a Car!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TestIComparable.Models;
+
+namespace TestIComparable
+{
+    // The System.IComparable interface specifies a behavior that allows an object to be sorted based on some
+    // specified key.Here is the formal definition:
+    // This interface allows an object to specify its relationship between other like objects.
+    //public interface IComparable
+    //{
+    //    int CompareTo(object o);
+    //}
+
+    class Program
+    {
+        static void Main()
+        {
+            Car[] myAutos = new Car[5];
+            myAutos[0] = new Car("Rusty", 80, 1);
+            myAutos[1] = new Car("Mary", 40, 234);
+            myAutos[2] = new Car("Viper", 40, 34);
+            myAutos[3] = new Car("Mel", 40, 4);
+            myAutos[4] = new Car("Chucky", 40, 5);
+
+            PrintCars(myAutos);
+
+            // The System.Array class defines a static method named Sort().
+            // When you invoke this method on an array of intrinsic types (int, short, string, etc.),
+            // you can sort the items in the array in numeric/alphabetic order,
+            // as these intrinsic data types implement IComparable.            
+            Array.Sort(myAutos);
+            PrintCars(myAutos);
+
+            // Now sort by pet name.
+            Array.Sort(myAutos, new PetNameComparer());
+            PrintCars(myAutos);
+
+            Console.ReadLine();
+        }
+
+        static public void PrintCars(Car[] cars)
+        {
+            Console.WriteLine("List of cars:");
+            foreach (Car c in cars)
+            {
+                Console.WriteLine("{0} {1}", c.CarID, c.PetName);
+            }
+            Console.WriteLine();
+        }
+    }
+}
